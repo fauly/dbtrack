9ac5f3cec66739763fc173673554e121bc3a5cdd
fauly <faulyfaux@gmail.com>
2025-01-27 18:14:08 +0000

    corrected import again

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,5 +4,7 @@ import sys
 # Add the application's directory to the Python path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Import the app object from main.pyca
-from app import app as application
+from app import create_app
+
+# Create the WSGI application object
+application = create_app()
